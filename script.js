c9ec7ccf7b0754d7657f56e0aaf092bd574cdbf4
Laurens.P <damianprida.dp@gmail.com>
2026-08-03 17:01:13 +0200

    clase jaba

---
+++ script.js
@@ -0,0 +1,18 @@
+
+console.log("Hello, World!");
+
+let 
+numeroRandom= Math.floor(Math.random() * 20) + 1;
+console.log(numeroRandom);
+
+for (let iteration = 1;
+    iteration <= 5;
+    iteration++) 
+    {let numeroUsuario = parseInt(prompt("Adivina el número entre 1 y 20:"));
+    }
+
+    function saludar (nombre){
+
+console.log("hola" + nombre + "!")
+}
+saludar (" damian")
