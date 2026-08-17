338a3c503973020e01f9b429d971e78357e50b44
Laurens.P <damianprida.dp@gmail.com>
2026-08-17 16:57:09 +0200

    clase 17/08

--- script.js
+++ script.js
@@ -1,88 +1,53 @@
-/* console.log ("nombres")
-let nombres = [" pepe"," juan"," maria"," carlos"," jose"," julieta"," ana"]
 
-/* let indice.pedro = nombres.indexOf("pedro") */
+/* const btn_click = document.getElementById("btn")
+
+
+function saludar (){
+    alert(Hola que tal?)
+}
+
+btn_click.addEventListener(
+    "click",
+    saludar
+) */
 /* 
-let indice_maria = nombres.indexOf("maria")
-nombres.splice(indice_maria, 1)
- */
-/* let indice_jose = nombres.indexOf("jose")
-nombres.splice("indice_jose", 1, "josesito") */
-
-/* const notas = [90, 40, 100]
-function calcularPromedio (numeros){
-    let sumaTotal = 0;
-    for (let numero of numeros) {
-        sumaTotal + numero;
-    }
-    let promedio = sumaTotal / numeros.length
-    return promedio
-} */
-let nombres = ['tv noblex', 'tv samsung', 'tv noblex']
-function contarNombres(lista, nombreBuscado){
-    let contador = 0;
-    for(const nombre of lista){
-         if(nombre === nombreBuscado){
-           contador++;
-    }
-    }
-    return contador;
-}
-
-//subir el precio de un determinado producto en una lista de stock
-
-console.log (contarNombres(nombres, "tv noblex"))
-
-function subirPrecio = (productos, id, precioASubir){
-    for(const producto of productos){
-        if(producto.id === idProducto){
-            producto.precio = producto.precio + precioASubir
-            break;
-        }
-    }
-}
-const producto = {
-    id: 1, 
-    titulo: 'Tv Samsung 52"',
-    precio: 4000
-}
-/* 
-Dado el producto escribir dentro de un elemento con id product-card
-h2: titulo
-span: precio
-Estructura: 
-    <h2></h2>
-    <div>
-        <span>Precio: <strong>$4000</strong></span>
-    </div>
-    <button>Comprar</button>
+Contador
+- El boton de resta decrementara tu contador
+- El boton de suma incrementara tu contador
+- El span mostrara el valor del contador (inicialmente 0)
+Recomendacion:
+- Tener una variable en JS con el valor de tu contador y esa variable mostrarla en el span
 */
-const mensajes = [
-    {
-        autor: 'pepe',
-        mensaje: "hola que tal!",
-        fecha: '21:30',
-        id: 1
-    },
-    {
-        autor: 'Tu',
-        mensaje: "Todo bien y vos?",
-        fecha: '21:32',
-        id: 2
-    },
-    {
-        autor: 'pepe',
-        mensaje: 'Genial!',
-        fecha: '21:35',
-        id: 3
-    }
-]
+
+
+
+const btnResta =document.getElementById("btn_cont_resta")
+const btnSuma =document.getElementById("btn_cont_suma")
+const spanContador=document. getElementById("contador")
+let contador = 0
+
+
+function sumar(){
+    contador= contador + 1;
+    spanContador.textContent= contador;
+}
+function restar(){
+    contador= contador - 1;
+    spanContador.textContent= contador;
+}
+
+btnResta.addEventListener (
+    "click", 
+    restar
+)
+btnSuma.addEventListener(
+    "click",
+    sumar
+)
 /* 
-Renderizar la lista de mensajes
-<div>
-    <h3>Autor</h3>
-    <p>Mensaje</p>
-    <span>Fecha</span>
-    <hr>
-</div>
+Selector de opciones
+Al clickear una opcion la seleccionare, 
+dependiendo de la opcion que seleccione en el div de opcion seleccionada 
+debe aparecer el numero de la opcion 
+(selecciono opcion 1 y debe decir "Opcion seleccionada: 1")
 */
