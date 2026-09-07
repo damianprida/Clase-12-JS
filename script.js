560c1515eabc32e2aa73ed79ff9aeae0c89d5ea9
Laurens.P <damianprida.dp@gmail.com>
2026-09-07 12:19:13 -0300

    js basico

--- script.js
+++ script.js
@@ -1,53 +1,18 @@
 
-/* const btn_click = document.getElementById("btn")
+/* Number(1) //1   
+Number(hola) // NaN
 
+boolean(50) // true
+boolean(0) // false */
 
-function saludar (){
-    alert(Hola que tal?)
+/* var nombre = "Damian"
+
+console.log("hola " + nombre) */
+
+let contraseña = prompt("Ingrese su contraseña")
+if (contraseña == "password123"){
+    console.log("inicio de sesion exitoso")
 }
-
-btn_click.addEventListener(
-    "click",
-    saludar
-) */
-/* 
-Contador
-- El boton de resta decrementara tu contador
-- El boton de suma incrementara tu contador
-- El span mostrara el valor del contador (inicialmente 0)
-Recomendacion:
-- Tener una variable en JS con el valor de tu contador y esa variable mostrarla en el span
-*/
-
-
-
-const btnResta =document.getElementById("btn_cont_resta")
-const btnSuma =document.getElementById("btn_cont_suma")
-const spanContador=document. getElementById("contador")
-let contador = 0
-
-
-function sumar(){
-    contador= contador + 1;
-    spanContador.textContent= contador;
+else {
+    console.log("Contraseña incorrecta")
 }
-function restar(){
-    contador= contador - 1;
-    spanContador.textContent= contador;
-}
-
-btnResta.addEventListener (
-    "click", 
-    restar
-)
-btnSuma.addEventListener(
-    "click",
-    sumar
-)
-/* 
-Selector de opciones
-Al clickear una opcion la seleccionare, 
-dependiendo de la opcion que seleccione en el div de opcion seleccionada 
-debe aparecer el numero de la opcion 
-(selecciono opcion 1 y debe decir "Opcion seleccionada: 1")
-*/
